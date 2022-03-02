81a818ec97f13ede3cc4812c6fdda1257bebd89a
alnicode32 <albensnico@gmail.com>
2022-03-02 16:17:16 -0500

    Añadido nuevos metodos para gestionar estudiantes

--- src/main/java/com/alnicode/students/service/StudentService.java
+++ src/main/java/com/alnicode/students/service/StudentService.java
@@ -4,6 +4,14 @@ import com.alnicode.students.model.Student;
 import java.util.List;
 
 public interface StudentService {
+    
     List<Student> getAllStudents();
+
     Student save(Student student);
+
+    Student getStudentById(Long studentId);
+
+    Student updateStudent(Student student);
+
+    void deleteStudent(Long studentId);
 }
