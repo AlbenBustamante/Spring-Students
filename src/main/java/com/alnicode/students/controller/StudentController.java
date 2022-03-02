e0a21cf7bf986987aa01324cb277463f4fe789aa
alnicode32 <albensnico@gmail.com>
2022-03-02 12:41:14 -0500

    Creado controlador base para estudiantes y metodo para devolver el html de la lista de estudiantes

---
+++ src/main/java/com/alnicode/students/controller/StudentController.java
@@ -0,0 +1,22 @@
+package com.alnicode.students.controller;
+
+import com.alnicode.students.service.StudentService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/students")
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/students")
+    public String getStudents(Model model) {
+        model.addAttribute("students", studentService.getAllStudents());
+        return "students";
+    }
+}
