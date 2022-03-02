6cebbe2612dbcdccbec98428dc5b2de58bbd1eca
alnicode32 <albensnico@gmail.com>
2022-03-02 11:29:18 -0500

    Creado el repositorio base para estudiantes

---
+++ src/main/java/com/alnicode/students/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.alnicode.students.repository;
+
+import com.alnicode.students.model.Student;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends CrudRepository<Student, Long> {
+    
+}
