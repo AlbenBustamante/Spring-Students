a4031befd6f823e852f9666ae508ae9a4ffe1b4f
alnicode32 <albensnico@gmail.com>
2022-03-02 11:13:54 -0500

    Creado el modelo de estudiantes

---
+++ src/main/java/com/alnicode/students/model/Student.java
@@ -0,0 +1,59 @@
+package com.alnicode.students.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
+    private Long studentId;
+
+    @Column(nullable = false, length = 40)
+    private String name;
+
+    @Column(nullable = false, length = 40)
+    private String lastName;
+
+    @Column(nullable = false, unique = true, length = 70)
+    private String email;
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
